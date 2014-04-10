a6302d8dc4de8f8fdaf7da3ca56008ad2fa11351
Teodoro Hochfarber <teohoch2@gmail.com>
2014-04-10 17:31:11 -0300

    Commit final SS Workshop, se agrega gitignore y se cambio atpy por AstroPy

--- README.md
+++ README.md
@@ -3,15 +3,26 @@
 
 Astronomical SYntetic Data Observatory
 
-Participantes:
-	*Rolando Casanueva
-	*Ricardo Carrasco
+Dependencias:
+    * scipy
+    * numpy
+    * matplotlib
+    * requests
+    * urllib3
+    * astropy (via pip)
 
 
 Participantes:
-    *Teodoro Hochfärber
-    *Renato Sanhueza
+    * Teodoro Hochfärber    (SS)
+    * Renato Sanhueza       (SS)
 
-    * Nicolas Bravo
-    * Juan Bustamante
+    * Nicolas Bravo         (IS)
+    * Juan Bustamante       (IS)
 
+    * Rolando Casanueva     (VU)
+	* Ricardo Carrasco      (VU)
+
+
+*******LOG SS*******
+    * Se cambio atpy por AstroPy, puesto que es la bibilioteca descendiente.
+    * Se agregaron .gitignore para .xlm y .fits
--- src/splataget.py
+++ src/splataget.py
@@ -5,30 +5,35 @@ import requests
 import atpy
 
 
-slap_serv='https://find.nrao.edu/splata-slap/slap'
-alma_band_freq={'3':[88,116],'4':[125,163],'6':[211,275],'7':[275,373],'8':[385,500],'9':[602,720]}
-segment_freq={'0':[1,50],'1':[50,100],'2':[100,150],'3':[150,200],'4':[200,250],'5':[250,300],'6':[300,350],'7':[350,400],'8':[400,450],\
-				  '9':[450,500],'10':[500,550],'11':[550,600],'12':[600,650],'13':[650,700],'14':[700,750],'15':[750,800],'16':[800,850],\
-				  '17':[850,900],'18':[900,950],'19':[950,1000]}
+slap_serv = 'https://find.nrao.edu/splata-slap/slap'
+alma_band_freq = {'3': [88, 116], '4': [125, 163], '6': [211, 275], '7': [275, 373], '8': [385, 500], '9': [602, 720]}
+segment_freq = {'0': [1, 50], '1': [50, 100], '2': [100, 150], '3': [150, 200], '4': [200, 250], '5': [250, 300],
+                '6': [300, 350], '7': [350, 400], '8': [400, 450], \
+                '9': [450, 500], '10': [500, 550], '11': [550, 600], '12': [600, 650], '13': [650, 700],
+                '14': [700, 750], '15': [750, 800], '16': [800, 850], \
+                '17': [850, 900], '18': [900, 950], '19': [950, 1000]}
 
-def getLines(log,prefix,freqs):
-   c=299792458.0
-   for band in freqs:
-      log.write('band='+band+'\n')
-      w_init=c/(freqs[band][0]*1000000000.0);
-      w_end= c/(freqs[band][1]*1000000000.0);
-      data = '?REQUEST=queryData&WAVELENGTH='+str(w_init)+'/'+str(w_end)+'&VERB=3'
-      curl=slap_serv + data.encode('utf-8')
-      log.write('  -> Downloading lines via SLAP:\n')
-      log.write('  -> '+curl+'\n')
-      req = urllib2.Request(curl)
-      response = urllib2.urlopen(req)
-      votable = response.read()
-      stri = prefix+band
-      location = './votables/'+stri+'.xml'
-      f = open(location, 'w')
-      f.write(votable)
-      f.close()
+
+def getLines(log, prefix, freqs):
+    c = 299792458.0
+    for band in freqs:
+        log.write('band=' + band + '\n')
+        w_init = c / (freqs[band][0] * 1000000000.0);
+        w_end = c / (freqs[band][1] * 1000000000.0);
+        data = '?REQUEST=queryData&WAVELENGTH=' + str(w_init) + '/' + str(w_end) + '&VERB=3'
+        curl = slap_serv + data.encode('utf-8')
+        log.write('  -> Downloading lines via SLAP:\n')
+        log.write('  -> ' + curl + '\n')
+        req = urllib2.Request(curl)
+        response = urllib2.urlopen(req)
+        votable = response.read()
+        stri = prefix + band
+        location = './votables/' + stri + '.xml'
+        f = open(location, 'w')
+        f.write(votable)
+        f.close()
+    print("Termine!")
+
 
 
 #def getAllLines(log):
@@ -46,6 +51,6 @@ def getLines(log,prefix,freqs):
 
 
 
-getLines(sys.stdout,'band',alma_band_freq)
+getLines(sys.stdout, 'band', alma_band_freq)
 #getLines(sys.stdout,'segment',segment_freq)
 
