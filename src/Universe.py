8ff5e35f9795379f0b03fd2663f74333a8af0d93
Mauricio Araya <maray@inf.utfsm.cl>
2014-08-12 12:47:05 -0400

    Removed people that do not contribute and clean

--- src/Universe.py
+++ src/Universe.py
@@ -9,22 +9,8 @@ from scipy import signal
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 
-
-#UniverseSpec = namedtuple('SynUniverse','profile band_freq band_noise inten_group inten_values iso_abun base_abun base_CO')
-#CubeSpec = namedtuple('CubeSpec', 'alpha delta freq ang_res ang_fov spe_res spe_bw')
-
-
-#defaultUniverse = UniverseSpec('default', \
-#                          {'3': [88, 116], '4': [125, 163], '6': [211, 275], '7': [275, 373], '8': [385, 500],'9': [602, 720]},\
-#                          {'3': 0.01, '4': 0.012, '6': 0.02, '7': 0.04, '8': 0.08, '9': 0.16},\
-#                          [('default'), ('COv=0'), ('13COv=0'), ('HCO+, HC3N, CS, C18O, CH3OH, N2H')],\
-#                          [[0.1, 2], [20, 60], [5, 20], [1, 10]],\
-#                          {'13C': 1.0 / 30, '18O': 1.0 / 60, '17O': 1.0/120, '34S': 1.0 / 30, '33S': 1.0 / 120,'13N': 1.0 / 30, 'D': 1.0 / 30},\
-#                          [10 ** -5, 10 ** -6],\
-#                          1.0)
-
 class Universe:
-    """Synthetic Cube Generation Class"""
+    """A synthetic universe where to put synthetic objects."""
 
     def __init__(self, log):
         self.log=log
